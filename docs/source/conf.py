084ced05c4dfc3d62f0b41efcd169bc0edd2efe5
HDanke <dankethegamer@gmail.com>
2025-09-10 21:20:42 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,10 +26,9 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 extensions += ['sphinx.ext.autodoc','sphinx.ext.napoleon']
-html_theme = 'furo'
 import os, sys
 sys.path.insert(0, os.path.abspath('../../src'))
 try:
