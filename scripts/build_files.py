ac9ba54a663a3cdf85876794c09cc690838e7f29
HDanke <dankethegamer@gmail.com>
2025-09-26 22:13:57 +0300

    Update build_files.py

--- scripts/build_files.py
+++ scripts/build_files.py
@@ -357,7 +357,7 @@ def try_convert_int(val):
 remap_table["object_id"] = remap_table["object_id"].apply(try_convert_int)
 remap_table['min'] = remap_table['min'].astype("Int64")
 remap_table['max'] = remap_table['max'].astype("Int64")
-remap_table.replace(False, np.nan, inplace=True)
+remap_table.replace(False, pd.NA, inplace=True)
 remap_table = remap_table.rename(columns={
     "property_id": "prop"    
     })
