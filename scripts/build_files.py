106d1de2fd31f9ba663a55fd0978c639f4b9b8a5
HDanke <dankethegamer@gmail.com>
2026-01-01 01:54:23 +0200

    .

--- scripts/build_files.py
+++ scripts/build_files.py
@@ -359,7 +359,6 @@ remap_table = remap_table.where(pd.notnull(remap_table), None)
 def try_convert_int(val):
     
     try:
-        if val == 0.0: print(int(val))
         return int(val)
     except (ValueError, TypeError):
         return val
@@ -368,7 +367,7 @@ def try_convert_int(val):
 remap_table["object_id"] = remap_table["object_id"].apply(try_convert_int)
 remap_table['min'] = remap_table['min'].astype("Int64")
 remap_table['max'] = remap_table['max'].astype("Int64")
-remap_table["default"] = remap_table['default'].astype("Int64")
+remap_table["default"] = remap_table['default'].apply(try_convert_int)
 remap_table.replace(False, pd.NA, inplace=True)
 remap_table.replace(float("nan"), pd.NA, inplace=True)
 remap_table = remap_table.rename(columns={
