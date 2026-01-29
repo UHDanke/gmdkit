39a1d26409858ab268ce2ca12b5921b01940015f
HDanke <dankethegamer@gmail.com>
2026-01-29 14:34:49 +0200

    Update build_files.py

--- scripts/build_files.py
+++ scripts/build_files.py
@@ -1,11 +1,13 @@
 from scripts.build_scripts import (
     build_id_rules,
     build_lvl_props,
+    build_list_props,
     build_obj_props,
     build_obj_ids    
     )
 
 build_id_rules.main()
 build_lvl_props.main()
+build_list_props.main()
 build_obj_props.main()
 build_obj_ids.main()
