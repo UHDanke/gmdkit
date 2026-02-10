9b97e3a4e60f7b5982c165a37e362518e24fef6c
Xtreme <luqmancraft145@gmail.com>
2026-02-09 23:31:55 -0500

    Fix linux path

--- src/gmdkit/constants/paths/save/__init__.py
+++ src/gmdkit/constants/paths/save/__init__.py
@@ -1,8 +1,17 @@
 from pathlib import Path
-from os import PathLike, getenv
+from os import getenv
+import sys
 
-LOCAL_PATH = Path(getenv("LOCALAPPDATA")) / "GeometryDash"
-GAME_MANAGER_PATH= LOCAL_PATH / "CCGameManager.dat"
-LOCAL_LEVELS_PATH = LOCAL_PATH / "CCLocalLevels.dat"
-MUSIC_LIBRARY_PATH = LOCAL_PATH / "musiclibrary.dat"
-SFX_LIBRARY_PATH = LOCAL_PATH / "sfxlibrary.dat"
+# Handle different platforms for GD save location
+if sys.platform == "win32":
+    LOCAL_PATH = Path(getenv("LOCALAPPDATA", "")) / "GeometryDash"
+else:
+    LOCAL_PATH = Path.home() / ".local" / "share" / "Steam" / "steamapps" / "common" / "Geometry Dash"
+
+if LOCAL_PATH.exists():
+    GAME_MANAGER_PATH = LOCAL_PATH / "CCGameManager.dat"
+    LOCAL_LEVELS_PATH = LOCAL_PATH / "CCLocalLevels.dat"
+    MUSIC_LIBRARY_PATH = LOCAL_PATH / "musiclibrary.dat"
+    SFX_LIBRARY_PATH = LOCAL_PATH / "sfxlibrary.dat"
+else:
+    raise FileNotFoundError(f"Base save directory not found: {LOCAL_PATH}")
